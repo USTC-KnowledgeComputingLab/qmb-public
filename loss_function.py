50782dd1fa7a025942162d8949645400604cf684
Hao Zhang <hzhangxyz@outlook.com>
2024-09-19 11:16:18 +0800

    Add another loss function.

--- loss_function.py
+++ loss_function.py
@@ -36,6 +36,19 @@ def target_filtered_log(a, b):
     return loss.mean()
 
 
+def sum_filtered_log(a, b):
+    # b is target, a is learnable
+    error = (a.log() - b.log()) / (2 * torch.pi)
+    error_real = error.real
+    error_imag = error.imag - error.imag.round()
+    loss = error_real**2 + error_imag**2
+    sum_a_b = a.abs() + b.abs()
+    loss = loss / (1 + 1e-10 / sum_a_b)
+    # This function scale only for very small abs value.
+    # I think we could ignore those definitly for amplitude less than 1e-10.
+    return loss.mean()
+
+
 @torch.jit.script
 def direct(a, b):
     # b is target, a is learnable
