80f000da2610eb8c5549f4a17b2f2dc60cb0762e
Hao Zhang <hzhangxyz@outlook.com>
2024-09-26 12:25:53 +0800

    Add pre commit config.

--- qmp/_ising.cpp
+++ qmp/_ising.cpp
@@ -1,12 +1,9 @@
-#include <array>
 #include <cstdint>
 #include <memory>
 #include <pybind11/complex.h>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <tuple>
-#include <utility>
 #include <vector>
 
 namespace py = pybind11;
