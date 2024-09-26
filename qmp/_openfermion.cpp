19e6f79cd8b2c929c23e53579da6a1d3afa8ff8e
Hao Zhang <hzhangxyz@outlook.com>
2024-09-26 17:17:27 +0800

    Move binary tree to a header file.

--- qmp/_openfermion.cpp
+++ qmp/_openfermion.cpp
@@ -1,6 +1,5 @@
 #include <array>
 #include <cstdint>
-#include <memory>
 #include <pybind11/complex.h>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
@@ -9,65 +8,9 @@
 #include <utility>
 #include <vector>
 
+#include "_binary_tree.hpp"
+
 namespace py = pybind11;
-
-// Binary tree, with each leaf associated with a value,
-// and we can set and get leaf value by an iterator.
-template<typename T, T miss>
-class Tree {
-    std::unique_ptr<Tree<T, miss>> _left;
-    std::unique_ptr<Tree<T, miss>> _right;
-    std::unique_ptr<T> _value;
-
-    T& value() {
-        if (!_value) {
-            _value = std::make_unique<T>(miss);
-        }
-        return *_value;
-    }
-
-
-  public:
-    template<typename It>
-    void set(It begin, It end, T v) {
-        if (begin == end) {
-            value() = v;
-        } else {
-            if (*(begin++)) {
-                if (!_right) {
-                    _right = std::make_unique<Tree<T, miss>>();
-                }
-                _right->set(begin, end, v);
-            } else {
-                if (!_left) {
-                    _left = std::make_unique<Tree<T, miss>>();
-                }
-                _left->set(begin, end, v);
-            }
-        }
-    }
-
-    template<typename It>
-    T get(It begin, It end) {
-        if (begin == end) {
-            return value();
-        } else {
-            if (*(begin++)) {
-                if (_right) {
-                    return _right->get(begin, end);
-                } else {
-                    return miss;
-                }
-            } else {
-                if (_left) {
-                    return _left->get(begin, end);
-                } else {
-                    return miss;
-                }
-            }
-        }
-    }
-};
 
 // Hamiltonian handle for openfermion data.
 // Every term of hamiltonian is operators less or equal than 4,
