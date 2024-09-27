9aa140692ef92b8bb783526ab8e3894feb899108
Hao Zhang <hzhangxyz@outlook.com>
2024-09-27 10:57:56 +0800

    Refect iter script, but it is not completed yet.

--- qmb/__main__.py
+++ qmb/__main__.py
@@ -1,12 +1,14 @@
 import tyro
 from .learn import LearnConfig
 from .vmc import VmcConfig
+from .iter import IterConfig
 
 
 def main():
     tyro.extras.subcommand_cli_from_dict({
         "learn": LearnConfig,
         "vmc": VmcConfig,
+        "iter": IterConfig,
     }).main()
 
 
