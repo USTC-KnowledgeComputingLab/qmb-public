bf120e02dc9a6adae8eadbfe207d11fbed202c5f
Hao Zhang <hzhangxyz@outlook.com>
2024-11-08 13:32:50 +0800

    Add docstring for version.py and __init__.py.

--- qmb/__init__.py
+++ qmb/__init__.py
@@ -1,3 +1,8 @@
+"""
+The qmb package helps you solve problems related to quantum many-body systems.
+For more details, check out the README.md file.
+"""
+
 __all__ = ["version", "__version__"]
 
 from .version import version, __version__
