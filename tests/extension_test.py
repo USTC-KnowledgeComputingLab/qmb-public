0a92b8aa11ed8682beafdc75e82f0abe6a621585
Hao Zhang <hzhangxyz@outlook.com>
2024-11-11 00:04:35 +0800

    Add pylint and mypy to pre-commit scripts.

--- tests/extension_test.py
+++ tests/extension_test.py
@@ -1,7 +1,17 @@
-import qmb
+"""
+Test module for PyTorch C++ extension.
+"""
+
+import torch
 import qmb.hamiltonian
-import sys
 
 
-def test_import():
-    extension = qmb.hamiltonian.get_extension()
+def test_import() -> None:
+    """
+    Test the import and availability of the PyTorch C++ extension operations.
+    """
+    # pylint: disable=protected-access
+    extension = qmb.hamiltonian.Hamiltonian._get_extension()
+    _ = getattr(extension, "prepare")
+    _ = torch.ops._hamiltonian.bose2
+    _ = torch.ops._hamiltonian.fermi
