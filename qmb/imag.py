866afec5e6358201b1b8e0ef5d6bd7df1964c26a
Hao Zhang <hzhangxyz@outlook.com>
2024-11-20 04:08:48 +0800

    Implement dynamic lanczos algorithm.

--- qmb/imag.py
+++ qmb/imag.py
@@ -11,32 +11,92 @@ import tyro
 from . import losses
 from .common import CommonConfig
 from .subcommand_dict import subcommand_dict
-from .utility import extend_with_select
+from .model_dict import ModelProto
 
 
-def _lanczos(hamiltonian: torch.Tensor, psi: torch.Tensor, step: int, threshold: float) -> tuple[torch.Tensor, torch.Tensor]:
-    v = [psi / torch.linalg.norm(psi)]  # pylint: disable=not-callable
-    alpha = torch.tensor([], dtype=hamiltonian.dtype.to_real(), device=hamiltonian.device)
-    beta = torch.tensor([], dtype=hamiltonian.dtype.to_real(), device=hamiltonian.device)
+class _DynamicLanczos:
+    """
+    This class implements the dynamic Lanczos algorithm for solving quantum many-body problems.
+    """
 
-    w = hamiltonian @ v[-1]
-    alpha = torch.cat((alpha, (v[-1].conj() @ w).real.reshape([1])), dim=0)
-    w = w - alpha[-1] * v[-1]
-    for _ in range(step):
-        norm_w = torch.linalg.norm(w)  # pylint: disable=not-callable
-        if norm_w < threshold:
-            break
-        beta = torch.cat((beta, norm_w.reshape([1])), dim=0)
-        v.append(w / beta[-1])
+    # pylint: disable=too-few-public-methods
+
+    def __init__(  # pylint: disable=too-many-arguments
+            self,
+            *,
+            model: ModelProto,
+            configs: torch.Tensor,
+            psi: torch.Tensor,
+            step: int,
+            threshold: float,
+            count_extend: int,
+    ):
+        self.model = model
+        self.configs = configs
+        self.psi = psi
+        self.step = step
+        self.threshold = threshold
+        self.count_extend = count_extend
+
+    def _hamiltonian(self) -> torch.Tensor:
+        logging.info("Constructing Hamiltonian...")
+        indices_i_and_j, values = self.model.inside(self.configs)
+        count_config = len(self.configs)
+        hamiltonian = torch.sparse_coo_tensor(indices_i_and_j.T, values, [count_config, count_config], dtype=torch.complex128).to_sparse_csr()
+        logging.info("Hamiltonian constructed")
+        return hamiltonian
+
+    def _extend(self, psi: torch.Tensor) -> None:
+        logging.info("Extending basis...")
+        indices_i_and_j, values, configs_extended = self.model.outside(self.configs)
+        count_core = len(self.configs)
+        count_extended = len(configs_extended)
+        hamiltonian = torch.sparse_coo_tensor(indices_i_and_j.T, values, [count_core, count_extended], dtype=torch.complex128).to_sparse_csc()
+
+        importance = (psi.conj() @ hamiltonian).abs()
+        importance[:count_core] += importance.max()
+
+        selected_indices = importance.sort(descending=True).indices[:count_core + self.count_extend].sort().values
+        count_selected = len(selected_indices)
+
+        self.configs = configs_extended[selected_indices]
+        self.psi = torch.nn.functional.pad(self.psi, (0, count_selected - count_core))
+        logging.info("Basis extended from %d to %d", count_core, count_selected)
+
+    def run(self, keep_until: int = -1) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Run the dynamic Lanczos algorithm.
+        """
+        hamiltonian = self._hamiltonian()
+
+        v = [self.psi / torch.linalg.norm(self.psi)]  # pylint: disable=not-callable
+        alpha = torch.tensor([], dtype=hamiltonian.dtype.to_real(), device=hamiltonian.device)
+        beta = torch.tensor([], dtype=hamiltonian.dtype.to_real(), device=hamiltonian.device)
+
+        if keep_until == -1:
+            self._extend(v[-1])
+            return self.run(keep_until=keep_until + 1)
         w = hamiltonian @ v[-1]
         alpha = torch.cat((alpha, (v[-1].conj() @ w).real.reshape([1])), dim=0)
-        w = w - alpha[-1] * v[-1] - beta[-1] * v[-2]
+        w = w - alpha[-1] * v[-1]
+        for i in range(self.step):
+            norm_w = torch.linalg.norm(w)  # pylint: disable=not-callable
+            if norm_w < self.threshold:
+                break
+            beta = torch.cat((beta, norm_w.reshape([1])), dim=0)
+            v.append(w / beta[-1])
+            if keep_until == i:
+                self._extend(v[-1])
+                return self.run(keep_until=keep_until + 1)
+            w = hamiltonian @ v[-1]
+            alpha = torch.cat((alpha, (v[-1].conj() @ w).real.reshape([1])), dim=0)
+            w = w - alpha[-1] * v[-1] - beta[-1] * v[-2]
 
-    # Currently, PyTorch does not support this functionality natively, so we resort to using SciPy for this operation.
-    vals, vecs = scipy.linalg.eigh_tridiagonal(alpha.cpu(), beta.cpu(), lapack_driver="stemr", select="i", select_range=[0, 0])
-    energy = torch.as_tensor(vals[0])
-    result = torch.sum(torch.as_tensor(vecs[:, 0]).to(device=hamiltonian.device) * torch.stack(v, dim=1), dim=1)
-    return energy, result
+        # Currently, PyTorch does not support this functionality natively, so we resort to using SciPy for this operation.
+        vals, vecs = scipy.linalg.eigh_tridiagonal(alpha.cpu(), beta.cpu(), lapack_driver="stemr", select="i", select_range=[0, 0])
+        energy = torch.as_tensor(vals[0])
+        result = torch.sum(torch.as_tensor(vecs[:, 0]).to(device=hamiltonian.device) * torch.stack(v, dim=1), dim=1)
+        return hamiltonian, energy, self.configs, result
 
 
 @dataclasses.dataclass
@@ -51,8 +111,8 @@ class ImaginaryConfig:
 
     # The sampling count
     sampling_count: typing.Annotated[int, tyro.conf.arg(aliases=["-n"])] = 4000
-    # The number of configurations to sample during post-sampling
-    post_sampling_count: typing.Annotated[int, tyro.conf.arg(aliases=["-c"])] = 0
+    # The extend count for the Krylov subspace
+    krylov_extend_count: typing.Annotated[int, tyro.conf.arg(aliases=["-c"])] = 400
     # The number of Krylov iterations to perform
     krylov_iteration: typing.Annotated[int, tyro.conf.arg(aliases=["-k"])] = 16
     # The threshold for the Krylov iteration
@@ -88,8 +148,8 @@ class ImaginaryConfig:
         logging.info(
             "Arguments Summary: "
             "Sampling Count: %d, "
-            "Post-Sampling Count: %d, "
-            "krylov Iteration: %.10f, "
+            "Krylov Extend Count: %d, "
+            "krylov Iteration: %d, "
             "krylov Threshold: %.10f, "
             "Loss Function: %s, "
             "Use LBFGS: %s, "
@@ -98,7 +158,7 @@ class ImaginaryConfig:
             "Local Loss Threshold: %.10f, "
             "Logging Psi: %d",
             self.sampling_count,
-            self.post_sampling_count,
+            self.krylov_extend_count,
             self.krylov_iteration,
             self.krylov_threshold,
             self.loss_name,
@@ -114,25 +174,19 @@ class ImaginaryConfig:
 
             logging.info("Sampling configurations")
             configs, psi, _, _ = network.generate_unique(self.sampling_count)
-            configs_count = len(configs)
-            logging.info("Sampling completed, pool size: %d", configs_count)
-
-            if self.post_sampling_count != 0:
-                logging.info("Extending configuration pool")
-                configs, psi = extend_with_select(model, configs, psi, self.post_sampling_count)
-                configs_count = len(configs)
-                logging.info("Configuration pool extended, new size: %d", configs_count)
-
-            logging.info("Calculating sparse data for the Hamiltonian matrix on the configurations.")
-            indices_i_and_j, values = model.inside(configs)
-            logging.info("Converting sparse matrix data into a sparse tensor.")
-            hamiltonian = torch.sparse_coo_tensor(indices_i_and_j.T, values, [configs_count, configs_count], dtype=torch.complex128).to_sparse_csr()
-            logging.info("Sparse Hamiltonian matrix on configurations has been created.")
+            logging.info("Sampling completed")
 
             logging.info("Computing the target for local optimization")
-            target_energy, targets = _lanczos(hamiltonian, psi, self.krylov_iteration, self.krylov_threshold)
-            max_index = targets.abs().argmax()
-            targets = targets / targets[max_index]
+            hamiltonian, target_energy, configs, psi = _DynamicLanczos(
+                model=model,
+                configs=configs,
+                psi=psi,
+                step=self.krylov_iteration,
+                threshold=self.krylov_threshold,
+                count_extend=self.krylov_extend_count,
+            ).run()
+            max_index = psi.abs().argmax()
+            psi = psi / psi[max_index]
             logging.info("Local optimization target calculated, the target energy is %.10f", target_energy.item())
 
             loss_func: typing.Callable[[torch.Tensor, torch.Tensor], torch.Tensor] = getattr(losses, self.loss_name)
@@ -147,7 +201,7 @@ class ImaginaryConfig:
                 optimizer.zero_grad()
                 amplitudes = network(configs)
                 amplitudes = amplitudes / amplitudes[max_index]
-                loss = loss_func(amplitudes, targets)
+                loss = loss_func(amplitudes, psi)
                 loss.backward()  # type: ignore[no-untyped-call]
                 loss.amplitudes = amplitudes.detach()  # type: ignore[attr-defined]
                 return loss
@@ -180,9 +234,9 @@ class ImaginaryConfig:
                 final_energy.item() - model.ref_energy,
             )
             logging.info("Displaying the largest amplitudes")
-            indices = targets.abs().sort(descending=True).indices
+            indices = psi.abs().sort(descending=True).indices
             for index in indices[:self.logging_psi]:
-                logging.info("Configuration: %s, Target amplitude: %s, Final amplitude: %s", "".join(map(str, configs[index].cpu().numpy())), f"{targets[index].item():.8f}",
+                logging.info("Configuration: %s, Target amplitude: %s, Final amplitude: %s", "".join(map(str, configs[index].cpu().numpy())), f"{psi[index].item():.8f}",
                              f"{amplitudes[index].item():.8f}")
 
 
