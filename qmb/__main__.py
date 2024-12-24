e6e06fa9c666a897eb7eb16aa340f2999365bcca
Hao Zhang <hzhangxyz@outlook.com>
2024-12-25 06:17:10 +0800

    Remove the learn script as we prefer to imag script.

--- qmb/__main__.py
+++ qmb/__main__.py
@@ -6,7 +6,6 @@ import tyro
 from . import openfermion as _  # type: ignore[no-redef]
 from . import fcidump as _  # type: ignore[no-redef]
 from . import ising as _  # type: ignore[no-redef]
-from . import learn as _  # type: ignore[no-redef]
 from . import vmc as _  # type: ignore[no-redef]
 from . import imag as _  # type: ignore[no-redef]
 from . import precompile as _  # type: ignore[no-redef]
